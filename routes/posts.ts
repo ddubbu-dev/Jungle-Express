3d33b378cbb1f743460ccc7c94912e85f20124c0
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-29 22:36:09 +0900

    feat: Add board Put API #7

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express'
 import Posts from '../schemas/post'
-import { PostCreateBody, PostDeleteBody } from './posts.type'
+import { PostCreateBody, PostDeleteBody, PostUpdateBody } from './posts.type'
 
 const router = Router()
 
@@ -22,8 +22,8 @@ router.post(
             user_name,
             content,
             user_pw: user_password,
-        }).then(() => {
-            res.status(201).send({})
+        }).then((post) => {
+            res.status(201).json({ id: post.id })
         })
     }
 )
@@ -47,8 +47,34 @@ router.delete(
                 .status(401)
                 .json({ message: '비밀번호 입력을 확인해주세요' })
 
-        await post
-            ?.deleteOne()
+        return post
+            .deleteOne()
+            .then(() => res.json({}))
+            .catch(() => res.status(500).json({ message: '서버 에러' }))
+    }
+)
+
+router.put(
+    '/',
+    async (
+        req: Request<object, object, PostUpdateBody>,
+        res: Response
+    ): Promise<any> => {
+        const { user_password, post_id, title, content } = req.body
+
+        const post = await Posts.findById(post_id)
+
+        if (!post)
+            return res.status(404).json({ message: '게시글을 찾을 수 없어요' })
+
+        // TODO: await bcrypt.compare(user_password, post.user_pw)
+        if (post.user_pw != user_password)
+            return res
+                .status(401)
+                .json({ message: '비밀번호 입력을 확인해주세요' })
+
+        return post
+            .updateOne({ title, content })
             .then(() => res.json({}))
             .catch(() => res.status(500).json({ message: '서버 에러' }))
     }
--- routes/posts.type.ts
+++ routes/posts.type.ts
@@ -9,3 +9,11 @@ export interface PostDeleteBody {
     user_password: string
     post_id: string
 }
+
+export interface PostUpdateBody {
+    user_password: string
+    post_id: string
+
+    title: string
+    content: string
+}
