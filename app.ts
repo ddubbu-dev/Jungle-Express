57ad34c08b68f1002d5b05a669dc697885635d76
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-29 19:47:46 +0900

    refactor: db connect 함수 분리

--- app.ts
+++ app.ts
@@ -1,15 +1,21 @@
 import express, { Express, Request, Response } from 'express'
-import { db_test } from './schemas'
+import { connect_db } from './schemas'
 
 const app: Express = express()
 const port = 8000
 
 app.get('/', async (req: Request, res: Response) => {
     res.send('Typescript + Node.js + Express Server + MongoDB')
-
-    await db_test().catch((err) => console.log(err))
 })
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at <https://localhost>:${port}`)
+app.listen(port, async () => {
+    await connect_db()
+        .then(() => {
+            console.log('[1] DB Connected')
+        })
+        .catch((err) => {
+            console.log('[Error] DB Connected\n', err)
+        })
+
+    console.log(`[2] Server runs at <https://localhost>:${port}`)
 })
