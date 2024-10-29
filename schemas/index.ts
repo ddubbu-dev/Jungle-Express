39d854add4dcf70cefedeefbf429b265128f6fb9
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-30 01:39:41 +0900

    feat: Add dot-env for managing 환경변수

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+dotenv.config()
 
 const db_username = process.env.MONGO_DB_USERNAME
 const db_password = process.env.MONGO_DB_PASSWORD
 const db_name = process.env.MONGO_DB_DATABASE
+
 const uri = `mongodb+srv://${db_username}:${db_password}@express-practice.q33pr.mongodb.net/${db_name}`
 
 const connect_db = () => mongoose.connect(uri)
