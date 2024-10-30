83fea89633ce886276a23330639adf4d92f79109
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-30 21:59:09 +0900

    chore: Add DELETE TABLE SQL

--- src/db/sql/common.sql
+++ src/db/sql/common.sql
@@ -1,3 +1,5 @@
--- DROP TABLE posts
--- DROP TABLE users
-DROP TABLE migrations
+-- DROP TABLE IF EXISTS posts;
+-- DROP TABLE IF EXISTS users;
+
+-- TRUNCATE TABLE posts;
+DELETE FROM users; --  Cannot truncate a table referenced in a foreign key constraint
